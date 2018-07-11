b0e6b636d6ceb9107d877b834acc4c9a6f9cc4cb
Adam Urbina <adamurbina@gmail.com>
2018-07-11 23:52:02 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,6 +21,7 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
       doc = Nokogiri::HTML(open(profile_url))
+      new_hash = {}
       links = doc.css(".social-icon-container").css("a")
       links.each {|link|
           url = link.attribute("href").value
