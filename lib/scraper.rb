e3f20518a394f2179a5791e19e889c3d1146b55a
Adam Urbina <adamurbina@gmail.com>
2018-07-11 23:50:19 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,7 +24,7 @@ class Scraper
       links = doc.css(".social-icon-container").css("a")
       links.each {|link|
           url = link.attribute("href").value
-          puts url.type
+          puts url.class.name
           case url
           when url.match('twitter')
               puts "twitter: "
