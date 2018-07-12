c59d26b00b3550440af4d990289a2a40bde12c50
Adam Urbina <adamurbina@gmail.com>
2018-07-12 00:12:37 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -39,7 +39,7 @@ class Scraper
       new_hash[:profile_quote] = doc.css(".profile_quote").text
       new_hash[:bio] = doc.css(".description-holder").css("p").text
 
-      return new_hash
+      puts new_hash
 
   end
 
